4532b0c84667a86694a04d01cf446de2de5a8d83
Christian Weissenbacher <christian-weissenbacher@gmx.at>
2020-11-02 21:16:10 +0100

    PUT and DELETE implemented
    PUT and DELETE is working
    
    POST was fixed so it can now more then one line can be saved in text file

--- REST-HTTP-Server/Response.cs
+++ REST-HTTP-Server/Response.cs
@@ -35,9 +35,17 @@ namespace REST_HTTP_Server
             {
                 return GetHandler(request);
             }
+            else if(request.Type =="DELETE")
+            {
+                return DeleteHanlder(request);
+            }
+           else if(request.Type == "PUT")
+            {
+                return PutHandler(request);
+            }
 
 
-            Response myResponse = new Response("text/plain", "200 Request sucess", "Dies ist mein scharfer Body");
+            Response myResponse = new Response("text/plain", "404 Not Found", "Wrong Input Man, just wrong");
 
             return myResponse;
 
@@ -53,15 +61,21 @@ namespace REST_HTTP_Server
                 return new Response("text/plain", "404 Not Found", "");
             }
             String body = "";
-            
-            
+
+            int bodystart = 0;
             for(int i=0; i < request.HeadRest.Length; i++)
             {
                 if (request.HeadRest[i] == "\r") 
                 {
-                    body = request.HeadRest[i + 1];
+                    bodystart = i+1;
+                    //body = request.HeadRest[i + 1];
                 }
             }
+            for (int a = bodystart; a < request.HeadRest.Length; a++) 
+            {
+                body += request.HeadRest[a];
+            }
+
             string ID = CreateNewFile(body, request.Order);
 
 
@@ -71,7 +85,7 @@ namespace REST_HTTP_Server
         }
        
 
-        //make the actual file and get the new id back
+        //----------------------make the actual file and get the new id back---------------------------
         private static string  CreateNewFile(string body, string path) 
         {
             string ID = "";
@@ -136,7 +150,7 @@ namespace REST_HTTP_Server
 
             int message = ValidOrder(request.Order); //check if GET request with special message (/messages/1) is valid and get the number of the message as int
             
-            if (message<0) //if ValidOrder is invalid int message will be -1 
+            if (message<0) //if ValidOrder is invalid  the int message will be -1 
             {
                 return new Response("text/plain", "404 Not Found", "");
             }
@@ -176,39 +190,7 @@ namespace REST_HTTP_Server
         }
 
 
-        //-----------check if Ordermessage is valid and give messagenumber as int back------------
-        private static int ValidOrder(string order)
-        {
-            String standard = "/messages";
-            int number = 0;
-            
-            if (order.Length<= standard.Length)
-            {
-                return -1;
-            }
-            
-            string[] collection = order.Split('/');
-            
-            if (collection[1] != "messages")
-            {
-                return -1;
-            }
-
-            //try to convert string into int
-            try                        
-            {
-                number = Int32.Parse(collection[2]);
-
-                return number;
-                
-            }
-            catch (FormatException) //if this fails return -1
-            {
-                Console.WriteLine($"Unable to parse '{collection[1]}'");
-                return -1;
-            }
-            
-        }
+       
         //----------------------------------------Gets on specific message---------------------------
         private static Response GetOne(int number)
         {
@@ -220,7 +202,7 @@ namespace REST_HTTP_Server
             FileInfo[] files = directory.GetFiles("*.txt");
             if (files.Length < number)
             {
-                body = "File Number " + number.ToString()  + "is not available";
+                body = "File Number " + number.ToString()  + " is not available";
                 return new Response("text/plain", "200 Request sucess", body);
             }
             else
@@ -231,7 +213,153 @@ namespace REST_HTTP_Server
         }
         //#######################################################################################################################
 
-        //create Server Response message
+
+        //##############################Functions to handle the DELETE Request###################################################
+
+
+        private static Response DeleteHanlder(Request request)
+        {
+            int number = ValidOrder(request.Order);
+
+            if (number<0)
+            {
+                return new Response("text/plain", "404 Not Found", "");
+            }
+            else 
+            {
+                return DeleteOne(number);
+            }
+            
+        }
+        //------------------------Delete the actual file-------------------------------------------- 
+        private static Response DeleteOne(int number) 
+        {
+            string body= "";
+            string path = "/messages";
+
+            string theWay = (AppDomain.CurrentDomain.BaseDirectory + path); //get the the path of the program (there also lies the message folder)
+            DirectoryInfo directory = new DirectoryInfo(theWay);
+            FileInfo[] files = directory.GetFiles("*.txt");
+            if (files.Length < number)
+            {
+                body = "File Number " + number.ToString() + " is not available";
+                return new Response("text/plain", "200 Request sucess", body);
+            }
+            else
+            {
+                File.Delete(files[number - 1].ToString());  //file gets deleted
+                body = "File Number " + number.ToString() + " is deleted";
+                return new Response("text/plain", "200 Request sucess", body);
+
+            }
+            
+        }
+
+
+
+        //###############################################################################################################
+
+
+        //###################################Functions that handle the PUT Request#######################################
+
+        //Hanldes the PUT Request over all
+        private static Response PutHandler(Request request)
+        {
+            int number = ValidOrder(request.Order); //check if PUT request with special message (/messages/1) is valid and get the number of the message as int
+
+            if (number < 0) //if ValidOrder is invalid  the int message will be -1 
+            {
+                return new Response("text/plain", "404 Not Found", "");
+            }
+
+            string body = "";
+
+            int bodystart = 0;
+            for (int i = 0; i < request.HeadRest.Length; i++)
+            {
+                if (request.HeadRest[i] == "\r")
+                {
+                    bodystart = i + 1;
+                    //body = request.HeadRest[i + 1];
+                }
+            }
+            for (int a = bodystart; a < request.HeadRest.Length; a++)
+            {
+                body += request.HeadRest[a];
+            }
+            return PutOne(number, body);
+            
+        }
+
+
+        private static Response PutOne(int number, string message) 
+        {
+            string path = "/messages";
+            string body = "";
+            string theWay = (AppDomain.CurrentDomain.BaseDirectory + path); //get the the path of the program (there also lies the message folder)
+            DirectoryInfo directory = new DirectoryInfo(theWay);
+            FileInfo[] files = directory.GetFiles("*.txt");
+            if (files.Length < number)
+            {
+                body = "File Number " + number.ToString() + " is not available";
+                return new Response("text/plain", "200 Request sucess", body);
+            }
+            else
+            {
+                File.WriteAllText(files[number-1].ToString(), message);
+                body = "File Number " + number.ToString() + " was changed";
+                return new Response("text/plain", "200 Request sucess", body);
+
+            }
+
+        }
+
+
+        //##############################################################################################################
+
+        //-----------check if Ordermessage is valid and give messagenumber as int back------------
+        private static int ValidOrder(string order)
+        {
+            String standard = "/messages";
+            int number = 0;
+
+            if (order.Length <= standard.Length)
+            {
+                return -1;
+            }
+
+            string[] collection = order.Split('/');
+
+            if (collection[1] != "messages")
+            {
+                return -1;
+            }
+
+            //try to convert string into int
+            try
+            {
+                number = Int32.Parse(collection[2]);
+
+                return number;
+
+            }
+            catch (FormatException) //if this fails return -1
+            {
+                Console.WriteLine($"Unable to parse '{collection[1]}'");
+                return -1;
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+        //--------------------------------create Server Response Headers message-----------------------------------
         public void ServerResponse(NetworkStream stream) 
         {
             int dataLength = Data.Length; 
